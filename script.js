12b8484b74c5c541d899ec888d90015fb97a97cb
penguin-listening <113907872+penguin-listening@users.noreply.github.com>
2022-11-07 10:16:35 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+$(function() {
+
+ $(window).scroll(function(){
+    const windowHeight = $(window).height();
+    const scroll = $(window).scrollTop();
+    
+    $('.aboutimg').each(function() {
+        const targetPosition = $(this).offset().top;
+        if (scroll > targetPosition - windowHeight + 100) {
+            $(this).addClass("img-fadein");
+            }
+        });
+    });
+
+$(window).scroll(function(){
+    const windowHeight = $(window).height();
+    const scroll = $(window).scrollTop();
+
+    $('.backgroundimg').each(function() {
+        const targetPosition = $(this).offset().top;
+        if (scroll > targetPosition - windowHeight + 100) {
+            $(this).addClass("img-fadein");
+        }
+    });
+});
+
+/トグルメニュー/ 
+$(function(){
+    $('#menu').click(function(){
+       $('ul').slideToggle(200);
+       $('nav').css('display',"block");
+    });
+});
+
+/alertbox/ 
+$(function(){
+    $('#bg').hide();
+
+    $('.btn').click(function(){
+        $('#bg').fadeIn(300);
+    });
+
+    $('#ok').click(function(){
+        $('#bg').fadeOut(300);
+    });
+});
+
+
+});
